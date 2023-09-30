7434f301fe9a8c0ea41838a3403df51a5205b5a5
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-09-30 22:42:10 +0900

    工夫１

--- src/app/components/Sidebar/BackNumbers/page.tsx
+++ src/app/components/Sidebar/BackNumbers/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
@@ -41,6 +42,157 @@ function BackNumbers() {
                     </Link>
         </button>   
       </div>
+
+      <div className='flex z-10 justify-center gap-20 mx-80  '>
+              {/*  */}
+              <div>
+                
+                <Link href={'/blogs/Blog6'} className=' text-orange-500 text-lg w-167 h-146 cursor-pointer hover:scale-105 active:scale-95 '>
+                     
+                      <Image 
+                        
+                        src='/image/flowers-7242099_640.jpg' 
+                        alt='image' 
+                        width={167}
+                        height={146}
+                        className=' hover:scale-110 active:scale-95'
+                         />
+                    
+                    {/* <br></br> */}
+                   
+                   
+                </Link>
+                <div className='py-2'>
+                   <button type='button' className=' bg-yellow-200 border border-yellow-200 rounded-md justify-items-end flex shadow-sm shadow-slate-500 hover:shadow-lg active:shadow-none hover:scale-110 active:scale-95'>
+                    <a href={'/blogs/Blog6'} className='text-orange-400 text-lg w-167 h-146 cursor-pointer text-center py-1 mx-2 justify-items-end flex'>
+                     投稿６
+                     </a>
+                   </button>
+                   </div>  
+                </div>
+              <div>
+                
+                <Link href={'/blogs/Blog7'} className=' text-orange-500 text-lg w-167 h-146 cursor-pointer hover:scale-105 active:scale-95 '>
+                     
+                      <Image 
+                        
+                        src='/image/flowers-7242099_640.jpg' 
+                        alt='image' 
+                        width={167}
+                        height={146}
+                        className=' hover:scale-110 active:scale-95'
+                         />
+                    
+                    {/* <br></br> */}
+                   
+                   
+                </Link>
+                <div className='py-2'>
+                   <button type='button' className=' bg-yellow-200 border border-yellow-200 rounded-md justify-items-end flex shadow-sm shadow-slate-500 hover:shadow-lg active:shadow-none hover:scale-110 active:scale-95'>
+                    <a href={'/blogs/Blog7'} className='text-orange-400 text-lg w-167 h-146 cursor-pointer text-center py-1 mx-2 justify-items-end flex'>
+                     投稿７
+                     </a>
+                   </button>
+                   </div>  
+                </div>
+              <div>
+                
+                <Link href={'/blogs/Blog8'} className=' text-orange-500 text-lg w-167 h-146 cursor-pointer hover:scale-105 active:scale-95 '>
+                     
+                      <Image 
+                        
+                        src='/image/flowers-7242099_640.jpg' 
+                        alt='image' 
+                        width={167}
+                        height={146}
+                        className=' hover:scale-110 active:scale-95'
+                         />
+                    
+                    {/* <br></br> */}
+                   
+                   
+                </Link>
+                <div className='py-2'>
+                   <button type='button' className=' bg-yellow-200 border border-yellow-200 rounded-md justify-items-end flex shadow-sm shadow-slate-500 hover:shadow-lg active:shadow-none hover:scale-110 active:scale-95'>
+                    <a href={'/blogs/Blog8'} className='text-orange-400 text-lg w-167 h-146 cursor-pointer text-center py-1 mx-2 justify-items-end flex'>
+                     投稿８
+                     </a>
+                   </button>
+                   </div>  
+                </div>
+              <div>
+                
+                <Link href={'/blogs/Blog4'} className=' text-orange-500 text-lg w-167 h-146 cursor-pointer hover:scale-105 active:scale-95 '>
+                     
+                      <Image 
+                        
+                        src='/image/flowers-7242099_640.jpg' 
+                        alt='image' 
+                        width={167}
+                        height={146}
+                        className=' hover:scale-110 active:scale-95'
+                         />
+                    
+                    {/* <br></br> */}
+                   
+                   
+                </Link>
+                <div className='py-2'>
+                   <button type='button' className=' bg-yellow-200 border border-yellow-200 rounded-md justify-items-end flex shadow-sm shadow-slate-500 hover:shadow-lg active:shadow-none hover:scale-110 active:scale-95'>
+                    <a href={'/blogs/Blog4'} className='text-orange-400 text-lg w-167 h-146 cursor-pointer text-center py-1 mx-2 justify-items-end flex'>
+                     投稿４
+                     </a>
+                   </button>
+                   </div>  
+                </div>
+              <div>
+                
+                <Link href={'/blogs/Blog5'} className=' text-orange-500 text-lg w-167 h-146 cursor-pointer hover:scale-105 active:scale-95 '>
+                     
+                      <Image 
+                        
+                        src='/image/flowers-7242099_640.jpg' 
+                        alt='image' 
+                        width={167}
+                        height={146}
+                        className=' hover:scale-110 active:scale-95'
+                         />
+                    
+                    {/* <br></br> */}
+                   
+                   
+                </Link>
+                <div className='py-2'>
+                   <button type='button' className=' bg-yellow-200 border border-yellow-200 rounded-md justify-items-end flex shadow-sm shadow-slate-500 hover:shadow-lg active:shadow-none hover:scale-110 active:scale-95'>
+                    <a href={'/blogs/Blog5'} className='text-orange-400 text-lg w-167 h-146 cursor-pointer text-center py-1 mx-2 justify-items-end flex'>
+                     投稿５
+                     </a>
+                   </button>
+                </div>  
+              </div>
+                
+                {/* <Link href={'/blogs/Blog2'} className=' text-orange-500 text-xs'>
+                   投稿２
+                </Link>
+                <Link href={'/blogs/Blog3'} className=' text-orange-500 text-xs'>
+                   投稿３
+                </Link>
+                <Link href={'/blogs/Blog4'} className=' text-orange-500 text-xs'>
+                   投稿４
+                </Link>
+                <Link href={'/blogs/Blog5'} className=' text-orange-500 text-xs'>
+                   投稿５
+                </Link> */}
+              
+                {/* <Blog2 />
+                <Blog3 />
+                <Blog4 />
+                <Blog5 /> */}
+
+              </div>      
+                <br />
+                <br />
+                <br />
       <div className='z-10 sm:text-4xl sm:mb-80 sm:mt-0 text-xl ml-10 '>
          <button type='button' className=' bg-pink-300  sm:px-20 sm:mx-80  rounded-md  hover:scale-105 scale-95 z-20'>
                     <Link href={'/components/Sidebar/BackNumbers/backNumber10'} className='z-30'>
@@ -51,7 +203,9 @@ function BackNumbers() {
                     </Link>
         </button>   
       </div>
-                
+      <div className='text-6xl ml-80 pl-20 text-blue-800'>
+         10月、ただいま準備中・・・・
+      </div>         
                
     </>        
     </div>
