358a38bfd166306aec4f69e6bf5efacf75f2737c
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-10-01 23:03:17 +0900

    a8Link修正

--- src/app/components/Sidebar/OtherGenres/page.tsx
+++ src/app/components/Sidebar/OtherGenres/page.tsx
@@ -40,16 +40,15 @@ const OtherGenres = () => {
         ただいま準備中・・・・
       </div>
              
-      
-         
-            <div className='Z-10 sm:m-80 mb-0 hover:scale-105 active:scale-95'>
+<br />         
+            <div className='sm:Z-10 sm:m-80 mb-0 hover:scale-105 active:scale-95'>
               <Link href={"https://px.a8.net/svt/ejp?a8mat=3HKIIT+2XZ78A+348+6L9O1"}>
               <a href="https://px.a8.net/svt/ejp?a8mat=3HKIIT+2XZ78A+348+6L9O1" rel="nofollow">
 <Image className=' border-0'  width="336" height="280" alt="" src="https://www29.a8.net/svt/bgt?aid=210909125178&wid=002&eno=01&mid=s00000000404001107000&mc=1"/></a>
 <Image className='border-0'  width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=3HKIIT+2XZ78A+348+6L9O1" alt=""/>
               </Link>
               <br />
-              <Link href={'href="https://px.a8.net/svt/ejp?a8mat=3HKN7U+BFEKKA+47L8+67Z9T"'}>
+              <Link href={"https://px.a8.net/svt/ejp?a8mat=3HKN7U+BFEKKA+47L8+67Z9T"}>
             <a href="https://px.a8.net/svt/ejp?a8mat=3HKN7U+BFEKKA+47L8+67Z9T" rel="nofollow">
 <Image className='border-0'   width="728" height="90" alt="" src="https://www21.a8.net/svt/bgt?aid=210915210691&wid=002&eno=01&mid=s00000019646001045000&mc=1"/></a>
 <Image className=' border-0'   width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=3HKN7U+BFEKKA+47L8+67Z9T" alt=""/>
