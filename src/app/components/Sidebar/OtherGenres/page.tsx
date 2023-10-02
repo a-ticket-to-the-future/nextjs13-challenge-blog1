542af80042ae6782b54545263c9c929f2a988119
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-10-02 22:35:10 +0900

    投稿１:メディカルアロマ

--- src/app/components/Sidebar/OtherGenres/page.tsx
+++ src/app/components/Sidebar/OtherGenres/page.tsx
@@ -43,7 +43,7 @@ const OtherGenres = () => {
 <br />  
        {/* ロリポップ */}
             <div className='sm:Z-10 z-50 sm:ml-80 sm:mb-0 hover:scale-105 active:scale-95'>
-              <Link href={"https://px.a8.net/svt/ejp?a8mat=3HKIIT+2XZ802+348+6L9O1"}>
+              <Link href={"https://px.a8.net/svt/ejp?a8mat=3HKIIT+2XZ802+348+6L9O1"} className='z-50'>
               <a href="https://px.a8.net/svt/ejp?a8mat=3HKIIT+2XZ802+348+6L9O1" rel="nofollow">
 <Image className=' border-0'  width="336" height="280" alt="" src="https://www29.a8.net/svt/bgt?aid=210909125178&wid=003&eno=01&mid=s00000000404001107000&mc=1"/></a>
 <Image className='border-0'  width="1" height="1" src="https://www18.a8.net/0.gif?a8mat=3HKIIT+2XZ802+348+6L9O1" alt=""/>
@@ -54,7 +54,7 @@ const OtherGenres = () => {
               {/* フリーナンス */}
               <div></div>
               <div className='sm:Z-10 z-50 sm:ml-80 sm:my-10 hover:scale-105 active:scale-95'>
-              <Link href={"https://px.a8.net/svt/ejp?a8mat=3HKN7U+BFEKKA+47L8+67Z9T"}>
+              <Link href={"https://px.a8.net/svt/ejp?a8mat=3HKN7U+BFELC2+47L8+67Z9T"}>
               <a href="https://px.a8.net/svt/ejp?a8mat=3HKN7U+BFELC2+47L8+67Z9T" rel="nofollow">
 <Image className='border-0'  width="728" height="90" alt="" src="https://www26.a8.net/svt/bgt?aid=210915210691&wid=003&eno=01&mid=s00000019646001045000&mc=1"/></a>
 <Image className='border-0' width="1" height="1" src="https://www13.a8.net/0.gif?a8mat=3HKN7U+BFELC2+47L8+67Z9T" alt=""/>
