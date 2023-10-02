e83c8446af8e5d4e40efc67f1dcad29860bce707
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-10-03 01:42:24 +0900

    zだった

--- src/app/blogs/Blog1/page.tsx
+++ src/app/blogs/Blog1/page.tsx
@@ -11,7 +11,7 @@ function Blog1() {
           <br></br>
           <br></br> */}
           <div className=' flex h-screen'>
-            <div className='sm:pl-6 pl-3 sm:ml-2 ml-0 mr-0'></div>
+            <div className='sm:pl-5 pl-3 sm:ml-3  ml-0 mr-0'></div>
           <div className=' w-1/6  h-screen'></div>
             <div className=' w-5/6 h-screen   '>
             <br></br>
@@ -65,9 +65,9 @@ function Blog1() {
                   あなた次第です。
                   </div>
                   
-                  <div className='sm:flex-col flex-col ml-1 my-1'>
+                  <div className='sm:flex-col flex-col sm:z-20 z-30 '>
                   {/* ロリポップ　中 */}
-                  <div className='sm:z-50 z-50 ml-50 hover:scale-105 active:scale-95'>
+                  <div className='sm:z-10 z-50 sm:ml-20 sm:mr-0 sm:mb-0 ml-30 mb-2 hover:scale-105 active:scale-95'>
                     <Link href={"https://px.a8.net/svt/ejp?a8mat=3HKIIT+2XZ802+348+6L9O1"} >
                       <a href="https://px.a8.net/svt/ejp?a8mat=3HKIIT+2XZ802+348+6L9O1" rel="nofollow">
                         <Image className='border-0'  width="336" height="280" alt="" src="https://www24.a8.net/svt/bgt?aid=210909125178&wid=003&eno=01&mid=s00000000404001107000&mc=1"/></a>
@@ -75,15 +75,15 @@ function Blog1() {
                     </Link>
                   </div>
                   {/* 365日記念日デコケーキ */}
-                  <div className='sm:z-50 z-50 ml-50 over:scale-105 active:scale-95'>
+                  <div className='sm:z-10 z-50 sm:ml-20 sm:mr-0 sm:mb-0 ml-30 my-2 hover:scale-105 active:scale-95'>
                     <Link href={"https://px.a8.net/svt/ejp?a8mat=3HR8H2+5DHXXE+426S+699KH"} >
-                      <a href="https://px.a8.net/svt/ejp?a8mat=3HR8H2+5DHXXE+426S+699KH" rel="nofollow" className='z-10 '>
+                      <a href="https://px.a8.net/svt/ejp?a8mat=3HR8H2+5DHXXE+426S+699KH" rel="nofollow">
                         <Image className='border-0'  width="300" height="250" alt="" src="https://www27.a8.net/svt/bgt?aid=211222694325&wid=003&eno=01&mid=s00000018946001051000&mc=1"/></a>
                         <Image className='border-0'  width="1" height="1" src="https://www10.a8.net/0.gif?a8mat=3HR8H2+5DHXXE+426S+699KH" alt=""/>
                     </Link>
                   </div>
                   {/* crowdtech　中　フリーランスでも会社員と同じように */}
-                  <div className='sm:z-50 z-50  ml-50 hover:scale-105 active:scale-95'>
+                  <div className='sm:z-10 z-50 sm:ml-20 sm:mr-0 sm:mb-0 ml-30 my-2 hover:scale-105 active:scale-95'>
                     <Link href={"https://px.a8.net/svt/ejp?a8mat=3HKOSC+633KXU+2OM2+ZW0CH"} >
                       <a href="https://px.a8.net/svt/ejp?a8mat=3HKOSC+633KXU+2OM2+ZW0CH" rel="nofollow">
                         <Image className='border-0'  width="300" height="250" alt="" src="https://www28.a8.net/svt/bgt?aid=210917244368&wid=003&eno=01&mid=s00000012521006028000&mc=1"/></a>
@@ -91,7 +91,7 @@ function Blog1() {
                     </Link>
                   </div>
                   {/* カップルリング 帯小　the kiss */}
-                  <div className='sm:z-50 z-50  ml-50 hover:scale-105 active:scale-95'>
+                  <div className='sm:z-10 z-50 sm:ml-20 sm:mr-0 sm:mb-0 ml-30 my-2 hover:scale-105 active:scale-95'>
                     <Link href={"https://px.a8.net/svt/ejp?a8mat=3N1YIC+59XCAQ+1O28+IISMP"} >
                       <a href="https://px.a8.net/svt/ejp?a8mat=3N1YIC+59XCAQ+1O28+IISMP" rel="nofollow">
                         <Image className='border-0'   width="300" height="250" alt="" src="https://www20.a8.net/svt/bgt?aid=220121076319&wid=003&eno=01&mid=s00000007784003111000&mc=1"/></a>
@@ -109,10 +109,10 @@ function Blog1() {
                   </div> */}
                 </div>
                 <br />
-                <div className='flex sm:ml-7 sm:mr-0 my-0 py-0 '>
+                <div className='flex sm:ml-2 sm:mr-10 my-0 py-0'>
                   
                   <button type='button' className=' z-10 bg-pink-300 sm:px-10 px-4 sm:ml-1 sm:mr-5 py-m my-0  rounded-lg hover:scale-105 active:scale-95'>
-                    <Link href={'/'} className=' z-10 sm:text-4xl text-xs text-slate-50'>
+                    <Link href={'/blogs/Blog5'} className=' z-10 sm:text-4xl text-xs text-slate-50'>
                       前の記事
                     </Link>
                   </button>
@@ -125,11 +125,11 @@ function Blog1() {
                    
                   
                   <button type='button' className=' z-10 bg-pink-300 sm:px-10 sm:mx-5 py-0 my-0 px-3 rounded-lg hover:scale-105 active:scale-95'>
-                    <Link href={'/blogs/Blog2'} className=' z-10 sm:text-4xl text-xs text-slate-50'>
+                    <Link href={'/blogs/Blog7'} className=' z-10 sm:text-4xl text-xs text-slate-50'>
                       次の記事
                     </Link>
                   </button> 
-          </div>
+          </div> 
           <br /> 
             </div>
           </div>
