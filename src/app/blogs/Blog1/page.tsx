ab09132751e4a5f9a0e935c9d2653fda1d39d822
a-ticket-to-the-future <tickets.to.the.future.katakura@gmail.com>
2023-10-03 01:12:14 +0900

    スマホ版調子悪い

--- src/app/blogs/Blog1/page.tsx
+++ src/app/blogs/Blog1/page.tsx
@@ -11,7 +11,7 @@ function Blog1() {
           <br></br>
           <br></br> */}
           <div className=' flex h-screen'>
-            <div className='sm:pl-8 pl-3 sm:ml-2 ml-0'></div>
+            <div className='sm:pl-6 pl-3 sm:ml-2 ml-0 mr-0'></div>
           <div className=' w-1/6  h-screen'></div>
             <div className=' w-5/6 h-screen   '>
             <br></br>
@@ -37,7 +37,7 @@ function Blog1() {
                 <br />
                 <br />
                 <div className=' flex  '>
-                  <div className=' sm:w-3/4 w-screen sm:text-xl text-xs h-300 border-4 text-blue-700 font-semibold overflow-wrap break-word  border-blue-700 p-5 '>
+                  <div className=' sm:w-3/4 w-screen sm:text-xl text-xs h-300 border-4 text-blue-700 font-semibold overflow-wrap break-word  border-blue-700 p-3 mr-0 '>
                   メディカルアロマとは、植物の香りの成分を使って、心と体の不調を改善したり、健康を維持したりする自然療法の一つです。メディカルアロマは、フランスやベルギーなどでは医療行為として認められており、医療や福祉の現場でも活用されています1。
 
                   メディカルアロマを学ぶことで、自分も含めて家族の健康を守る手助けができたり、不調に苦しむ誰かにあなた自身が手を差し伸べることができたら、それはとても素敵なことですよね？
@@ -51,7 +51,7 @@ function Blog1() {
                   <div>
                     <div></div>
                   {/* メディカルアロマ 大 */}
-                  <div className='sm:Z-10 z-50 sm:mb-0  hover:scale-105 active:scale-95'>
+                  <div className='sm:z-10 z-50 sm:mb-0  hover:scale-105 active:scale-95'>
                     <Link href={"https://px.a8.net/svt/ejp?a8mat=3HKPL2+14HJGI+4N8K+61Z81"}>
                     <a href="https://px.a8.net/svt/ejp?a8mat=3HKPL2+14HJGI+4N8K+61Z81" rel="nofollow" className='z-50'>
                       <Image className='border-1'  width="600" height="600" alt="" src="https://www21.a8.net/svt/bgt?aid=210918278068&wid=003&eno=01&mid=s00000021674001017000&mc=1"/></a>
@@ -64,9 +64,10 @@ function Blog1() {
                   これを機に誰かの役に立てる、自身の変化を手に入れるかどうかは
                   あなた次第です。
                   </div>
-                  <div className='sm:flex-col flex-col'>
+                  
+                  <div className='sm:flex-col flex-col ml-1 my-1'>
                   {/* ロリポップ　中 */}
-                  <div className='sm:Z-10 z-50 sm:ml-20 sm:mr-0 sm:mb-0 ml-50 hover:scale-105 active:scale-95'>
+                  <div className='sm:z-50 z-50 ml-50 hover:scale-105 active:scale-95'>
                     <Link href={"https://px.a8.net/svt/ejp?a8mat=3HKIIT+2XZ802+348+6L9O1"} >
                       <a href="https://px.a8.net/svt/ejp?a8mat=3HKIIT+2XZ802+348+6L9O1" rel="nofollow">
                         <Image className='border-0'  width="336" height="280" alt="" src="https://www24.a8.net/svt/bgt?aid=210909125178&wid=003&eno=01&mid=s00000000404001107000&mc=1"/></a>
@@ -74,15 +75,15 @@ function Blog1() {
                     </Link>
                   </div>
                   {/* 365日記念日デコケーキ */}
-                  <div className='sm:Z-10 z-50 sm:ml-20 sm:mr-0 sm:mb-0 ml-50 hover:scale-105 active:scale-95'>
+                  <div className='sm:z-50 z-50 ml-50 over:scale-105 active:scale-95'>
                     <Link href={"https://px.a8.net/svt/ejp?a8mat=3HR8H2+5DHXXE+426S+699KH"} >
-                      <a href="https://px.a8.net/svt/ejp?a8mat=3HR8H2+5DHXXE+426S+699KH" rel="nofollow">
+                      <a href="https://px.a8.net/svt/ejp?a8mat=3HR8H2+5DHXXE+426S+699KH" rel="nofollow" className='z-10 '>
                         <Image className='border-0'  width="300" height="250" alt="" src="https://www27.a8.net/svt/bgt?aid=211222694325&wid=003&eno=01&mid=s00000018946001051000&mc=1"/></a>
                         <Image className='border-0'  width="1" height="1" src="https://www10.a8.net/0.gif?a8mat=3HR8H2+5DHXXE+426S+699KH" alt=""/>
                     </Link>
                   </div>
                   {/* crowdtech　中　フリーランスでも会社員と同じように */}
-                  <div className='sm:Z-10 z-50 sm:ml-20 sm:mr-0 sm:mb-0 ml-50 hover:scale-105 active:scale-95'>
+                  <div className='sm:z-50 z-50  ml-50 hover:scale-105 active:scale-95'>
                     <Link href={"https://px.a8.net/svt/ejp?a8mat=3HKOSC+633KXU+2OM2+ZW0CH"} >
                       <a href="https://px.a8.net/svt/ejp?a8mat=3HKOSC+633KXU+2OM2+ZW0CH" rel="nofollow">
                         <Image className='border-0'  width="300" height="250" alt="" src="https://www28.a8.net/svt/bgt?aid=210917244368&wid=003&eno=01&mid=s00000012521006028000&mc=1"/></a>
@@ -90,7 +91,7 @@ function Blog1() {
                     </Link>
                   </div>
                   {/* カップルリング 帯小　the kiss */}
-                  <div className='sm:Z-10 z-50 sm:ml-20 sm:mr-0 sm:mb-0 ml-50 hover:scale-105 active:scale-95'>
+                  <div className='sm:z-50 z-50  ml-50 hover:scale-105 active:scale-95'>
                     <Link href={"https://px.a8.net/svt/ejp?a8mat=3N1YIC+59XCAQ+1O28+IISMP"} >
                       <a href="https://px.a8.net/svt/ejp?a8mat=3N1YIC+59XCAQ+1O28+IISMP" rel="nofollow">
                         <Image className='border-0'   width="300" height="250" alt="" src="https://www20.a8.net/svt/bgt?aid=220121076319&wid=003&eno=01&mid=s00000007784003111000&mc=1"/></a>
@@ -108,7 +109,7 @@ function Blog1() {
                   </div> */}
                 </div>
                 <br />
-                <div className='flex sm:ml-2 sm:mr-10 my-0 py-0'>
+                <div className='flex sm:ml-7 sm:mr-0 my-0 py-0 '>
                   
                   <button type='button' className=' z-10 bg-pink-300 sm:px-10 px-4 sm:ml-1 sm:mr-5 py-m my-0  rounded-lg hover:scale-105 active:scale-95'>
                     <Link href={'/'} className=' z-10 sm:text-4xl text-xs text-slate-50'>
